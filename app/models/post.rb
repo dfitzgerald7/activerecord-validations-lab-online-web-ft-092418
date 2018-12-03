ab92cdda89cd349db8ea735677057924350f8e9d
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-12-03 13:58:09 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 11}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
 end
